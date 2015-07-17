dd24c7b4268d22d8675c0317a8c770673c556ade
Kyle Zollinger <zollingerkj@gmail.com>
2015-07-17 15:16:48 -0600

    fixed profile links, splash page style

--- public/controllers/splashCtrl.js
+++ public/controllers/splashCtrl.js
@@ -5,10 +5,15 @@ app.controller('splashCtrl', function($scope, $location) {
 	$scope.showText1 = true;
 	$scope.showText2 = false;
 
-	(function () {
+	var changeText = function () {
 		window.setInterval(function() {
+			console.log('hey', $scope.showText1, $scope.showText2);
 			$scope.showText1 = !$scope.showText1;
 			$scope.showText2 = !$scope.showText2;
+			console.log('ho', $scope.showText1, $scope.showText2);
+			$scope.$apply();
 		}, 2000)
-	})();
+	}
+
+	changeText();
 })
